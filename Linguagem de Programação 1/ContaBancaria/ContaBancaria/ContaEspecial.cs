a9f60ad836dd0491a94d83e8481b33aea63294ed
Iago Lucas <lagoaqlagoai@hotmail.com>
2023-08-10 21:22:21 -0300

    send first program to github

---
+++ Linguagem de Programação 1/ContaBancaria/ContaBancaria/ContaEspecial.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2222
+{
+    public class ContaEspecial : contaBancaria
+    {
+        private double limite;
+
+
+
+        public ContaEspecial() : base()
+        {
+            limite = 0.0;
+        }
+
+        public double Limite
+        {
+            get { return limite; }
+            set { limite = value; }
+        }
+
+        public override void Abertura(double depositoInicial)
+        {
+            string msg = null;
+            base.Abertura(depositoInicial);
+            try
+            {
+                Console.WriteLine("Digite o Limite da Conta: ");
+                Limite = Convert.ToDouble(Console.ReadLine());
+                msg = "Limite Cadastrado com Sucesso!";
+
+            }
+            catch (Exception erro)
+            {
+                msg = "Limite Não Cadastrado com Sucesso \n" +
+                    "Erro: " + erro;
+            }
+            finally
+            {
+                Console.WriteLine(msg);
+            }
+        }
+
+        public override void Sacar(double valor)
+        {
+            if ((base.Saldo + Limite) >= valor)
+            {
+                base.Sacar(valor);
+
+            }
+            else
+            {
+                Console.WriteLine("Saldo Insuficiente!");
+            }
+        }
+    }
+
+}
+
+
---
+++ Linguagem de Programação 1/ContaBancaria/ContaBancaria/contaBancaria.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2222
+{
+    public class contaBancaria
+    {
+        private string nomeCliente;
+        private string nroConta;
+        private double saldo;
+
+
+        public contaBancaria()
+        {
+            nomeCliente = null;
+            nroConta = null;
+            saldo = 0.0;
+        }
+
+        public string NomeCliente
+        {
+            get { return nomeCliente; }
+            set { nomeCliente = value; }
+        }
+
+        public string NroConta
+        {
+            get { return nroConta; }
+            set { nroConta = value; }
+        }
+
+        public double Saldo
+        {
+            get { return saldo; }
+            set { saldo = value; }
+        }
+
+
+        public virtual void Abertura(double depositoInicial)
+        {
+            Saldo = depositoInicial;
+        }
+
+        public void Depositar(double valor)
+        {
+            Saldo += valor;
+        }
+
+        public virtual void Sacar(double valor)
+        {
+            Saldo -= valor;
+        }
+
+        public double verificaSaldo()
+        {
+            return Saldo;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
