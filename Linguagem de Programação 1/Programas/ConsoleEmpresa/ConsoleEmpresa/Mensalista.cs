e04ea976858e70ce42ba8cd39002d7187c3d2644
Iago Lucas <lagoaqlagoai@hotmail.com>
2023-08-24 19:45:39 -0300

    send

---
+++ Linguagem de Programação 1/Programas/ConsoleEmpresa/ConsoleEmpresa/Mensalista.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleEmpresa
+{
+    class Mensalista : Funcionario
+    {
+        public int QdeHoraExtra { get; set; }
+        public double ValorHoraExtra { get; set; }
+
+        public Mensalista() : base()
+        {
+            QdeHoraExtra = 0;
+            ValorHoraExtra = 0.0;
+        }
+
+        public override void LerDadosFuncionario()
+        {
+            base.LerDadosFuncionario();
+            try
+            {
+                Console.Write("Quantidade de Horas Extras: ");
+                QdeHoraExtra = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Valor da Hora Extra: ");
+                ValorHoraExtra = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Tente novamente! \nErro: " + ex);
+            }
+        }
+
+        public override string MostrarFuncionario()
+        {
+            return base.MostrarFuncionario() +
+                "\nQuantidade de Horas Extras Trabalhadas: " + QdeHoraExtra +
+                "\n Valor da Hora Extra: " + ValorHoraExtra +
+                "\nSalário Bruto: R$ " + CalcularSalarioBruto() +
+                "\nSalário Liquido: R$ " + CalcularSalarioLiquido();
+        }
+
+        public override double CalcularSalarioBruto()
+        {
+            return base.SalarioBase + QdeHoraExtra * ;
+        }
+
+        public override double CalcularSalarioLiquido()
+        {
+            return CalcularSalarioBruto() - (CalcularSalarioBruto() * 0.20);
+        }
+    }
+}
