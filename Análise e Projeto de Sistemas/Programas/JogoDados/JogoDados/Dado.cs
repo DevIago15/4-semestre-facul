f570b8f19791743f0a82a8918fe9df5440aff906
Iago Lucas <lagoaqlagoai@hotmail.com>
2023-08-29 23:40:39 -0300

    send new att

---
+++ Análise e Projeto de Sistemas/Programas/JogoDados/JogoDados/Dado.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JogoDados
+{
+    class Dado
+    {
+        public int Valor { get; set; }
+
+        public Dado()
+        {
+            Valor = 0;
+        }
+
+        public iniciarDado()
+        {
+
+        }
+
+
+    }
+}
---
+++ Análise e Projeto de Sistemas/Programas/JogoDados/JogoDados/Jogador.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JogoDados
+{
+    class Jogador
+    {
+        public string NomeJogador { get; set; }
+        public Dado d1 { get; set; }
+        public Dado d2{ get; set; }
+
+        public Jogador()
+        {
+            NomeJogador = null;
+            d1 = d1;
+            d2 = d2;
+        }
+
+
+        public void criarJogador()
+        {
+            Console.WriteLine("Escreva o Nome do Jogador: ");
+            NomeJogador = Console.ReadLine();
+        }
+        
+        public int Jogar()
+        {
+            int soma = d1.rolar() + d2.rolar();
+            return soma;
+        }
+    }
+
+
+}
